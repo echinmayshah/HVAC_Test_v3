c26daf3e84c5dd312aeb8fed74dd9e8f55cc577f
chinmay.shah <chinmay.shah@acldigital.com>
2025-11-19 17:01:50 +0530

    2th commit_in_dev_0_0

--- temp_sensor.c
+++ temp_sensor.c
@@ -93,10 +93,10 @@ uint16_t MCU_TempSensorCalc(uint16_t ADCResult)
 	{
 		divisor = 16.427272;
 	}
-	else if(ADCResult <= 1947 && ADCResult > 1669)	//120 and above
+/*2th commit_in_dev_0_0	else if(ADCResult <= 1947 && ADCResult > 1669)	//120 and above
 	{
 		divisor = 18.248f;
-	}
+	}	*/
 	/*1th commit_in_dev_0_0	else 
 	{
 		return(130);
